8ac48513abd05cfd8b9c25f2ab15b802f980f3b1
Jeremy Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-23 11:36:12 +1300

    Set up components

---
+++ client/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logo () {
+  return (
+    <div>
+      <h1>This is the logo</h1>
+    </div>
+  )
+}
---
+++ client/components/Task.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Task () {
+  return (
+    <div>
+      <h1>This will be a task</h1>
+    </div>
+  )
+}
