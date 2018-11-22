b083dc2eb9a4770235b487f5b208285221b4fbdb
Chris Laupama <chrislaupama@icloud.com>
2018-11-23 12:05:31 +1300

    started addtask func and reformatted state

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,20 +5,31 @@ import AddButton from './AddButton'
 
 export default class App extends Component {
   state = {
-    task = [{
+    task: [{
       id: 1,
       task: 'Drink a bottle of beer'
     },
     {
       id: 2,
       task: 'Get the BBQ roaring'
+    },
+    {
+      id: 3,
+      task: 'Go to the pub!'
     }]
   }
+
+  addNewTask = task => {
+    taskId = this.state.task.length
+
+  }
+
+
   render () {
     return (
       <div>
         <Logo />
-        <TaskList />
+        <TaskList taskList={this.state.task}/>
         <AddButton />
       </div>
     )
