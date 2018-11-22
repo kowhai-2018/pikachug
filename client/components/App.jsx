e477f02114131db86a7dc623dd4f4544f9294a4d
Jeremy Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-23 11:10:56 +1300

    initial setup

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <h1>React development has begun!</h1>
+    <div>
+      <h1>React development has begun!</h1>
+      <img src='https://pm1.narvii.com/5848/f4f8a6cfe7174e11a596bcb453a5eba0d2ab4e94_hq.jpg' />
+    </div>
   )
 }
 
 export default App
-
