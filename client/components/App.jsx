0c96f39c5c6b94a71b2fc4ffe4b428d9e0be1577
Chris Laupama <chrislaupama@icloud.com>
2018-11-23 11:44:37 +1300

    created state example

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,6 +4,16 @@ import TaskList from './TaskList'
 import AddButton from './AddButton'
 
 export default class App extends Component {
+  state = {
+    task = {
+      id: 1,
+      task: 'Drink a bottle of beer'
+    },
+    {
+      id: 2,
+      task: 'Get the BBQ roaring'
+    }
+  }
   render () {
     return (
       <div>
