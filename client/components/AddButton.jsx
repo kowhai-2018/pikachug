b21348eba7c983078b52627989040614d6ebacdc
Jeremy Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-23 15:35:03 +1300

    styling

--- client/components/AddButton.jsx
+++ client/components/AddButton.jsx
@@ -6,9 +6,10 @@ export default class AddButton extends Component {
   render () {
     return (
       <div>
+        <img src='https://fontmeme.com/permalink/181123/a56f071d104e21766f658a56e158d3ea.png' />
         <Modal
           header='Modal Header'
-          trigger={<a className='box' ></a>}>
+          trigger={<a className='box' ></a>}>          
           <AddForm add={this.props.add} />
         </Modal>
       </div>
