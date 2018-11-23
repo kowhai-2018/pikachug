70254fd8dd4ec91380416d32c87050b0ac6e454d
Alice Elizabeth Webb <alliewebb@gmail.com>
2018-11-23 15:32:03 +1300

    added checkbox to task

--- client/components/Task.jsx
+++ client/components/Task.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { Input } from 'react-materialize'
 
-export default function Task (props) {
-  return (
-    <div>
-      <ul>{props.task} - <button onClick={() => { props.removeTask(props.id) }}>Delete</button>
-      </ul>
-    </div>
-  )
+export default class Task extends Component {
+  state = {
+    id: this.props.id,
+    task: this.props.task,
+    checked: this.props.checked
+  }
+
+  changeHandler = e => {
+    this.setState({[e.target.name]: e.target.value})
+    this.props.confirmTask(this.state)
+    this.setState({
+      id: this.props.id,
+      task: this.props.task,
+      checked: this.props.checked
+    })
+  }
+
+  render () {
+    return (
+      <div className="container center">
+        <div className="col s8">
+          <ul>
+            <Input name='checked' checked={this.props.checked} type='checkbox' value='true' label={this.props.task} onClick={() => { this.changeHandler }} />
+            <i className="material-icons" onClick={() => { this.props.removeTask(this.props.id) }}>delete</i>
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
