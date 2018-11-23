1993aac7772a072a94c6e95b0bcfd29cf3b2255a
Jeremy Jonas <jeremy.jonas@hotmail.co.nz>
2018-11-23 14:40:20 +1300

    Added modal form function

--- client/components/AddButton.jsx
+++ client/components/AddButton.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import AddForm from './AddForm'
+import { Modal, Button } from 'react-materialize'
 
 export default class AddButton extends Component {
   render () {
     return (
       <div>
-        <h1>This will be the add button</h1>
-        <AddForm add={this.props.add}/>
+        <Modal
+          header='Modal Header'
+          trigger={<Button waves='light'>Add a Task</Button>}>
+          <AddForm add={this.props.add} />
+        </Modal>
       </div>
     )
   }
