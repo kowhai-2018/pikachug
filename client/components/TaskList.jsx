05d10a1b89674344eb8b41262cadc34db5eed862
Alice Elizabeth Webb <alliewebb@gmail.com>
2018-11-23 15:31:34 +1300

    passing new functions and removed unneeded code

--- client/components/TaskList.jsx
+++ client/components/TaskList.jsx
@@ -2,16 +2,10 @@ import React from 'react'
 import Task from './Task'
 
 export default function TaskList (props) {
-  // removeTask(index, event) {
-  //   const tasks = this.state.tasks
-  //   tasks.splice(index, 1)
-  //   this.setState({tasks})
-  // }
-
   return (
-    <div>
+    <div className="row">
       {props.taskList.map(task =>
-        (<Task id={task.id} task={task.task} key={task.id} removeTask={props.removeTask} />)
+        (<Task id={task.id} task={task.task} key={task.id} removeTask={props.removeTask} confirmTask={props.confirmTask} checked={props.checked} />)
       )}
     </div>
   )
