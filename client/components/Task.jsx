fa5e3113d22631bbf24f42cbac10ef127b37506f
Alice Elizabeth Webb <alliewebb@gmail.com>
2018-11-23 14:22:50 +1300

    added remove button to task

--- client/components/Task.jsx
+++ client/components/Task.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Task (props) {
   return (
     <div>
-      <ul>{props.task}</ul>
+      <ul>{props.task} - <button onClick={() => { props.removeTask(props.id) }}>Delete</button>
+      </ul>
     </div>
   )
 }
